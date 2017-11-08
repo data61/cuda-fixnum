a4c2a33fa47643b62fc62354339c1309f5548fa9
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2017-11-08 20:38:48 +1100

    Split utils.cu into primitives.cu and subwarp.cu.

--- src/subwarp.cu
+++ src/subwarp.cu
@@ -1,117 +1,5 @@
-#ifndef CUDA_MPMA_UTILS_CU
-#define CUDA_MPMA_UTILS_CU
-
-/*
- * PTX Assembler primitives
- */
-
-// TODO: Understand circumstances in which I might want to make this
-// "#define ASM asm __volatile__".
-//#define ASM asm
-
-// hi * 2^32 + lo = a * b
-__device__ __forceinline__ void
-umul(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b)
-{
-    // TODO: Measure performance difference between this and the
-    // equivalent:
-    //   mul.hi.u32 %0, %2, %3
-    //   mul.lo.u32 %1, %2, %3
-    asm ("{\n\t"
-         " .reg .u64 tmp;\n\t"
-         " mul.wide.u32 tmp, %2, %3;\n\t"
-         " mov.b64 { %0, %1 }, tmp;\n\t"
-         "}"
-         : "=r"(hi), "=r"(lo)
-         : "r"(a), "r"(b));
-}
-
-__device__ __forceinline__ void
-umul(uint64_t &r, uint32_t a, uint32_t b)
-{
-    asm ("mul.wide.u32 %0, %1, %2;"
-         : "=l"(r)
-         : "r"(a), "r"(b));
-}
-
-// hi * 2^64 + lo = a * b
-__device__ __forceinline__ void
-umul(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b)
-{
-    asm ("mul.hi.u64 %0, %2, %3;\n\t"
-         "mul.lo.u64 %1, %2, %3;"
-         : "=l"(hi), "=l"(lo)
-         : "l"(a), "l"(b));
-}
-
-// r = a * b + c
-__device__ __forceinline__ void
-umad(uint64_t &r, uint32_t a, uint32_t b, uint32_t c)
-{
-    asm ("mad.wide.u32 %0, %1, %2, %3;"
-         : "=l"(r)
-         : "r"(a), "r"(b), "r"(c));
-}
-
-// (hi, lo) = a * b + c
-__device__ __forceinline__ void
-umad(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b, uint32_t c)
-{
-    asm ("{\n\t"
-         " .reg .u64 tmp;\n\t"
-         " mad.wide.u32 tmp, %2, %3, %4;\n\t"
-         " mov.b64 { %0, %1 }, tmp;\n\t"
-         "}"
-         : "=r"(hi), "=r"(lo)
-         : "r"(a), "r"(b), "r"(c));
-}
-
-// (hi, lo) = a * b + c
-__device__ __forceinline__ void
-umad(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b, uint64_t c)
-{
-    asm ("mad.lo.cc.u64 %1, %2, %3, %4;\n\t"
-         "madc.hi.u64 %0, %2, %3, 0;"
-         : "=l"(hi), "=l"(lo)
-         : "l"(a), "l" (b), "l"(c));
-}
-
-// lo = a * b + c (mod 2^64)
-__device__ __forceinline__ void
-umad_lo(uint64_t &lo, uint64_t a, uint64_t b, uint64_t c)
-{
-    asm ("mad.lo.u64 %0, %1, %2, %3;"
-         : "=l"(lo)
-         : "l"(a), "l" (b), "l"(c));
-}
-
-__device__ __forceinline__ void
-umad_hi(uint64_t &hi, uint64_t a, uint64_t b, uint64_t c)
-{
-    asm ("mad.hi.u64 %0, %1, %2, %3;"
-         : "=l"(lo)
-         : "l"(a), "l" (b), "l"(c));
-}
-
-// as above but with carry in cy
-__device__ __forceinline__ void
-umad_lo_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c)
-{
-    asm ("mad.lo.cc.u64 %0, %2, %3, %4;\n\t"
-         "addc.u64 %1, %1, 0;"
-         : "=l"(lo), "+l"(cy)
-         : "l"(a), "l" (b), "l"(c));
-}
-
-__device__ __forceinline__ void
-umad_hi_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c)
-{
-    asm ("mad.hi.cc.u64 %0, %2, %3, %4;\n\t"
-         "addc.u64 %1, %1, 0;"
-         : "=l"(lo), "+l"(cy)
-         : "l"(a), "l" (b), "l"(c));
-}
-
+#ifndef CUDA_MPMA_SUBWARP_CU
+#define CUDA_MPMA_SUBWARP_CU
 
 /*
  * SUBWARPS:
@@ -280,7 +168,6 @@ struct subwarp
         int srcLane = (L + delta) & (width - 1);
         return __shfl(var, srcLane, width);
     }
-
 
     /*
      * The next three functions extend the usual shuffle functions to 64bit
