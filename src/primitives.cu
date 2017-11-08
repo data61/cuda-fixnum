473430cec8d725e85d4f7565332b1d3b048db5a8
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2017-11-08 21:02:14 +1100

    Move braces up.

--- src/primitives.cu
+++ src/primitives.cu
@@ -11,8 +11,7 @@
 
 // hi * 2^32 + lo = a * b
 __device__ __forceinline__ void
-umul(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b)
-{
+umul(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b) {
     // TODO: Measure performance difference between this and the
     // equivalent:
     //   mul.hi.u32 %0, %2, %3
@@ -27,8 +26,7 @@ umul(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b)
 }
 
 __device__ __forceinline__ void
-umul(uint64_t &r, uint32_t a, uint32_t b)
-{
+umul(uint64_t &r, uint32_t a, uint32_t b) {
     asm ("mul.wide.u32 %0, %1, %2;"
          : "=l"(r)
          : "r"(a), "r"(b));
@@ -36,8 +34,7 @@ umul(uint64_t &r, uint32_t a, uint32_t b)
 
 // hi * 2^64 + lo = a * b
 __device__ __forceinline__ void
-umul(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b)
-{
+umul(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b) {
     asm ("mul.hi.u64 %0, %2, %3;\n\t"
          "mul.lo.u64 %1, %2, %3;"
          : "=l"(hi), "=l"(lo)
@@ -46,8 +43,7 @@ umul(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b)
 
 // r = a * b + c
 __device__ __forceinline__ void
-umad(uint64_t &r, uint32_t a, uint32_t b, uint32_t c)
-{
+umad(uint64_t &r, uint32_t a, uint32_t b, uint32_t c) {
     asm ("mad.wide.u32 %0, %1, %2, %3;"
          : "=l"(r)
          : "r"(a), "r"(b), "r"(c));
@@ -55,8 +51,7 @@ umad(uint64_t &r, uint32_t a, uint32_t b, uint32_t c)
 
 // (hi, lo) = a * b + c
 __device__ __forceinline__ void
-umad(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b, uint32_t c)
-{
+umad(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b, uint32_t c) {
     asm ("{\n\t"
          " .reg .u64 tmp;\n\t"
          " mad.wide.u32 tmp, %2, %3, %4;\n\t"
@@ -68,8 +63,7 @@ umad(uint32_t &hi, uint32_t &lo, uint32_t a, uint32_t b, uint32_t c)
 
 // (hi, lo) = a * b + c
 __device__ __forceinline__ void
-umad(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b, uint64_t c)
-{
+umad(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b, uint64_t c) {
     asm ("mad.lo.cc.u64 %1, %2, %3, %4;\n\t"
          "madc.hi.u64 %0, %2, %3, 0;"
          : "=l"(hi), "=l"(lo)
@@ -78,16 +72,14 @@ umad(uint64_t &hi, uint64_t &lo, uint64_t a, uint64_t b, uint64_t c)
 
 // lo = a * b + c (mod 2^64)
 __device__ __forceinline__ void
-umad_lo(uint64_t &lo, uint64_t a, uint64_t b, uint64_t c)
-{
+umad_lo(uint64_t &lo, uint64_t a, uint64_t b, uint64_t c) {
     asm ("mad.lo.u64 %0, %1, %2, %3;"
          : "=l"(lo)
          : "l"(a), "l" (b), "l"(c));
 }
 
 __device__ __forceinline__ void
-umad_hi(uint64_t &hi, uint64_t a, uint64_t b, uint64_t c)
-{
+umad_hi(uint64_t &hi, uint64_t a, uint64_t b, uint64_t c) {
     asm ("mad.hi.u64 %0, %1, %2, %3;"
          : "=l"(lo)
          : "l"(a), "l" (b), "l"(c));
@@ -95,8 +87,7 @@ umad_hi(uint64_t &hi, uint64_t a, uint64_t b, uint64_t c)
 
 // as above but with carry in cy
 __device__ __forceinline__ void
-umad_lo_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c)
-{
+umad_lo_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c) {
     asm ("mad.lo.cc.u64 %0, %2, %3, %4;\n\t"
          "addc.u64 %1, %1, 0;"
          : "=l"(lo), "+l"(cy)
@@ -104,8 +95,7 @@ umad_lo_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c)
 }
 
 __device__ __forceinline__ void
-umad_hi_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c)
-{
+umad_hi_cc(uint64_t &lo, uint64_t &cy, uint64_t a, uint64_t b, uint64_t c) {
     asm ("mad.hi.cc.u64 %0, %2, %3, %4;\n\t"
          "addc.u64 %1, %1, 0;"
          : "=l"(lo), "+l"(cy)
