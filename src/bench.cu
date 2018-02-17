4893ef64ec690e516ebca504b9572e7b6e72efc3
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-02-17 15:58:28 +1100

    Memory management oddities; output arrays; new device op.

--- src/bench.cu
+++ src/bench.cu
@@ -1,4 +1,4 @@
-// -*- compile-command: "nvcc -std=c++11 -Xcompiler -Wall,-Wextra -g -G -lineinfo -gencode arch=compute_50,code=sm_50 -o bench bench.cu" -*-
+// -*- compile-command: "nvcc -Wno-deprecated-declarations -std=c++11 -Xcompiler -Wall,-Wextra -g -G -gencode arch=compute_50,code=sm_50 -o bench bench.cu" -*-
 
 #include <iostream>
 #include <iomanip>
@@ -13,7 +13,7 @@
 using namespace std;
 
 template< typename fixnum_impl >
-struct set_const : public Managed {
+struct set_const /*: public Managed*/ {
     // FIXME: The repetition of this is dumb and annoying
     typedef typename fixnum_impl::fixnum fixnum;
 
@@ -32,7 +32,7 @@ struct set_const : public Managed {
 
     ~set_const() {
         // FIXME: Why does this break?
-        cuda_free(bytes);
+        //cuda_free(bytes);
     }
 
     __device__ void operator()(fixnum &s) {
@@ -45,7 +45,7 @@ struct set_const : public Managed {
 // in a policy-based design
 // (https://en.wikipedia.org/wiki/Policy-based_design).
 template< typename fixnum_impl >
-struct ec_add : public Managed {
+struct ec_add /*: public Managed*/ {
     typedef typename fixnum_impl::fixnum fixnum;
 
     set_const<fixnum_impl> set_k;
@@ -53,16 +53,42 @@ struct ec_add : public Managed {
     ec_add(/* ec params */ long k = 17) : set_k(k) { }
 
     __device__ void operator()(fixnum &r, fixnum a, fixnum b) {
-        set_k(r);
-        fixnum_impl::mul_lo(r, a, b);
+        fixnum k;
+        set_k(k);
+        fixnum_impl::mul_lo(r, a, k);
+        fixnum_impl::mul_lo(r, r, b);
+        fixnum_impl::mul_lo(r, r, r);
+        fixnum_impl::mul_lo(r, r, r);
+    }
+};
+
+template< typename fixnum_impl >
+struct increments /*: public Managed*/ {
+    typedef typename fixnum_impl::fixnum fixnum;
+    long k;
+    increments(long k_ = 17) : k(k_) { }
+
+    __device__ void operator()(fixnum &r, fixnum a) {
+        int cy;
+        r = a;
+        for (long i = 0; i < k; ++i)
+            fixnum_impl::incr_cy(r, cy);
     }
 };
 
 static string fixnum_as_str(const uint8_t *fn, int nbytes) {
     ostringstream ss;
-    ss << setfill('0') << setw(2) << hex;
-    for (int i = nbytes - 1; i >= 0; --i)
-        ss << (int)fn[i];
+
+    for (int i = nbytes - 1; i >= 0; --i) {
+        // These IO manipulators are forgotten after each use;
+        // i.e. they don't apply to the next output operation (whether
+        // it be in the next loop iteration or in the conditional
+        // below.
+        ss << setfill('0') << setw(2) << hex;
+        ss << (int)fn[i] << flush;
+        if (i && !(i & 3))
+            ss << ' ';
+    }
     return ss.str();
 }
 
@@ -104,7 +130,7 @@ int main(int argc, char *argv[]) {
     typedef fixnum_array<fixnum_impl> fixnum_array;
 
     auto res = fixnum_array::create(n);
-    auto arr1 = fixnum_array::create(n, 5);
+    auto arr1 = fixnum_array::create(n, ~0UL);
     auto arr2 = fixnum_array::create(n, 245);
 
     cout << "res  = " << res << endl;
@@ -119,11 +145,13 @@ int main(int argc, char *argv[]) {
     // where each element is only 0 or 1? Need fixnum_array to have
     // C-array semantics, hence allowing other C arrays to be used
     // alongside, so pass an int* to collect the carries.
-//    fixnum_array::map(ec_add<fixnum_impl>(), res, arr1, arr2);
 
-    // cout << "res  = " << res << endl;
-    // cout << "arr1 = " << arr1 << endl;
-    // cout << "arr2 = " << arr2 << endl;
+    //fixnum_array::map(ec_add<fixnum_impl>(), res, arr1, arr2);
+    fixnum_array::map(increments<fixnum_impl>(1), res, arr1);
+
+    cout << "res  = " << res << endl;
+    cout << "arr1 = " << arr1 << endl;
+    cout << "arr2 = " << arr2 << endl;
 
     delete res;
     delete arr1;
