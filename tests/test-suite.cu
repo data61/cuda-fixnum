ed3cced8a6a33fb3ea552e61daf16307a29e1cb6
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-09 13:26:06 +1000

    Initial test suite.

---
+++ tests/test-suite.cu
@@ -0,0 +1,65 @@
+// -*- compile-command: "nvcc -I../src -ccbin clang-3.8 -Wno-deprecated-declarations -std=c++11 -lineinfo -Xcompiler -Wall,-Wextra -gencode arch=compute_50,code=sm_50 -o test-suite test-suite.cu -lstdc++ -lgtest" -*-
+
+#include <gtest/gtest.h>
+#include "array/fixnum_array.h"
+#include "fixnum/default.cu"
+#include "functions/square.cu"
+
+TEST(basic_arithmetic, square) {
+    typedef default_fixnum_impl<16> fixnum_impl;
+    typedef fixnum_array<fixnum_impl> fixnum_array;
+
+    int n = 1;
+
+    auto res = fixnum_array::create(n);
+    auto arr1 = fixnum_array::create(n, 0UL);
+    auto arr2 = fixnum_array::create(n, ~0UL);
+    auto arr3 = fixnum_array::create(n, 245);
+    auto sqr = new square<fixnum_impl>();
+    
+    fixnum_array::map(sqr, res, arr1);
+    // check that res is all zeros
+    constexpr int fn_bytes = fixnum_impl::FIXNUM_BYTES;
+    constexpr int bufsz = 4096;
+    uint8_t arr[bufsz];
+    int nelts;
+    for (auto &a : arr) a = 0;
+    res->retrieve_all(arr, bufsz, &nelts);
+    EXPECT_EQ(nelts - res->length(), 0);
+    // NB: Only checking up to fn_bytes because n = 1;
+    for (int i = 0; i < fn_bytes; ++i)
+        EXPECT_EQ(arr[i], 0);
+
+    fixnum_array::map(sqr, res, arr2);
+    res->retrieve_all(arr, bufsz, &nelts);
+    EXPECT_EQ(nelts - res->length(), 0);
+    uint8_t res2[] = { 1, 0, 0, 0, 0, 0, 0, 0, 254, 255, 255, 255, 255, 255, 255, 255 };
+    // NB: Only checking up to fn_bytes because n = 1;
+    for (int i = 0; i < fn_bytes; ++i)
+        EXPECT_EQ(arr[i], res2[i]);
+
+    fixnum_array::map(sqr, res, arr3);
+    res->retrieve_all(arr, bufsz, &nelts);
+    EXPECT_EQ(nelts - res->length(), 0);
+    EXPECT_EQ(arr[0], 121);
+    EXPECT_EQ(arr[1], 234);
+    // NB: Only checking up to fn_bytes because n = 1;
+    for (int i = 2; i < fn_bytes; ++i)
+        EXPECT_EQ(arr[i], 0);
+
+    delete sqr;
+    delete res;
+    delete arr1;
+    delete arr2;
+    delete arr3;
+}
+
+int main(int argc, char *argv[])
+{
+    int r;
+
+    testing::InitGoogleTest(&argc, argv);
+    r = RUN_ALL_TESTS();
+
+    return r;
+}
