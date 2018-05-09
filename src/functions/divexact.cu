e80da2abcd05cc9e7dfce148963764f6a7b828ed
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-09 11:50:06 +1000

    Fix divexact.

--- src/functions/divexact.cu
+++ src/functions/divexact.cu
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "util/managed.cu"
+
 template< typename fixnum_impl >
-struct divexact {
+struct divexact : public managed {
     typedef typename fixnum_impl::fixnum fixnum;
 
     // FIXME: It would be better to set bi here.
@@ -10,7 +12,7 @@ struct divexact {
     /*
      * q = a / b, assuming b divides a. bi must be 1/B (mod 2^(NBITS/2))
      * where NBITS := SLOT_WIDTH*FIXNUM_BITS.  bi is nevertheless treated as an
-     * NBITS intmod, so its hi half must be all zeros.
+     * NBITS fixnum, so its hi half must be all zeros.
      *
      * Source: MCA Algorithm 1.10.
      */
@@ -18,11 +20,21 @@ struct divexact {
         fixnum t, w = 0;
 
         // NBITS := SLOT_WIDTH*FIXNUM_BITS
+
         // w <- a bi  (mod 2^(NBITS / 2))
-        // FIXME: Original has half width:
-        //digit_mullo(w, a, bi, width / 2);
+
+        // FIXME: This is wasteful since we only want the bottom half of the
+        // result. Could we do something like:
+        //
+        //   create half_fixnum which is fixnum_impl< FIXNUM_BYTES / 2 > but
+        //   with same slot_layout. Then use half_fixnum::mul_lo(w, a, bi)
+        //
         fixnum_impl::mul_lo(w, a, bi);
-        // t <- b w    (mod 2^NBITS)
+        w = (slot_layout::laneIdx() < slot_layout::WIDTH / 2) ? w : 0;
+
+        // TODO: Can use the "middle product" to speed this up a
+        // bit. See MCA Section 1.4.5.
+        // t <- b w (mod 2^NBITS)
         fixnum_impl::mul_lo(t, b, w);
         // t <- a - b w (mod 2^NBITS)
         fixnum_impl::sub_br(t, a, t);
