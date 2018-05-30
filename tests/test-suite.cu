df2e3e1dbe6e9725941ca2a722f2fafc0cf6888c
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-30 10:56:56 +1000

    Run tests from precomputed test cases.

--- tests/test-suite.cu
+++ tests/test-suite.cu
@@ -1,7 +1,173 @@
 #include <gtest/gtest.h>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include "sexp.h"
+
+// FIXME FIXME!
+#include "sexp.cc"
+
 #include "array/fixnum_array.h"
 #include "fixnum/default.cu"
 #include "functions/square.cu"
+
+using namespace std;
+
+void die_if(bool p, const char *msg) {
+    if (p) {
+        cerr << "Error: " << msg << endl;
+        abort();
+    }
+}
+
+// should be a tuple
+//typedef tuple<byte_array, byte_array, byte_array> binop_args;
+typedef vector<byte_array> binop_args;
+
+class extract_all_args : public visitor {
+    vector<binop_args> args;
+    bool in_list = false;
+
+    vector<byte_array> current_arg;
+    bool expecting_atom = false;
+
+public:
+    virtual ~extract_all_args() { }
+
+    virtual void visit(const list &lst) override {
+        if (in_list) {
+            die_if(expecting_atom, "Got a list when expecting an atom.");
+            expecting_atom = true;
+        } else {
+            in_list = true;
+        }
+    }
+
+    virtual void visit(const atom &el) override {
+        die_if( ! expecting_atom, "Got an atom when expecting a list.");
+        current_arg.push_back(el.data);
+        if (current_arg.size() == 3) {
+            //binop_args t = make_tuple(current_arg[0], current_arg[1], current_arg[2]);
+            args.push_back(current_arg);
+            current_arg.clear();
+            expecting_atom = false;
+        }
+    }
+
+    vector<binop_args> result() {
+        return args;
+    }
+};
+
+vector<binop_args> fname_to_args(const char *fname) {
+    ifstream file(fname);
+    die_if ( ! file.good(), "Couldn't open file.");
+    auto s = unique_ptr<const sexp>(sexp::create(file));
+    extract_all_args v;
+    s->accept(v);
+    auto res = v.result();
+    cout << "Read " << res.size() << " args." << endl;
+    return res;
+}
+
+::testing::AssertionResult arrays_are_equal(
+        const uint8_t *expected, size_t expected_len,
+        const uint8_t *actual, size_t actual_len) {
+    if (expected_len > actual_len) {
+        return ::testing::AssertionFailure()
+            << "arrays don't have the same length";
+    }
+    size_t i;
+    for (i = 0; i < expected_len; ++i) {
+        if (expected[i] != actual[i]) {
+            return ::testing::AssertionFailure()
+                << "arrays differ: expected[" << i << "] = "
+                << static_cast<int>(expected[i])
+                << " but actual[" << i << "] = "
+                << static_cast<int>(actual[i]);
+        }
+    }
+    for (; i < actual_len; ++i) {
+        if (actual[i] != 0) {
+            return ::testing::AssertionFailure()
+                << "arrays differ: expected[" << i << "] = 0"
+                << " but actual[" << i << "] = "
+                << static_cast<int>(actual[i]);
+        }
+    }
+    return ::testing::AssertionSuccess();
+}
+
+template< typename fixnum_array >
+fixnum_array *read_arg(const byte_array &arg) {
+    return arg.size() > 0
+        ? fixnum_array::create(arg.data(), arg.size(), arg.size())
+        : fixnum_array::create(1, 0);
+}
+
+template< typename fixnum_impl >
+struct add_cy : public managed {
+    typedef typename fixnum_impl::fixnum fixnum;
+
+    __device__ void operator()(fixnum &r, fixnum a, fixnum b) {
+        fixnum_impl::add_cy(r, a, b);
+    }
+};
+
+class add_cy_test : public ::testing::TestWithParam<binop_args> { };
+
+static int skipped = 0;
+
+TEST_P(add_cy_test, basic) {
+    static constexpr int FIXNUM_BYTES = 256;
+    typedef default_fixnum_impl<FIXNUM_BYTES, uint64_t> fixnum_impl;
+    typedef fixnum_array<fixnum_impl> fixnum_array;
+
+    //const byte_array x, y, expected;
+    //tie(x, y, expected) = GetParam();
+    const binop_args &args = GetParam();
+    const byte_array &x = args[0];
+    const byte_array &y = args[1];
+    const byte_array &expected = args[2];
+
+    // FIXME: Should handle these cases somehow.
+    if (x.size() > FIXNUM_BYTES
+            || y.size() > FIXNUM_BYTES
+            || expected.size() > FIXNUM_BYTES) {
+        ++skipped;
+        return;
+    }
+
+    int n = 1;
+    auto res = fixnum_array::create(n);
+    auto xx = read_arg<fixnum_array>(x);
+    auto yy = read_arg<fixnum_array>(y);
+    auto fn = new add_cy<fixnum_impl>();
+
+    fixnum_array::map(fn, res, xx, yy);
+
+    constexpr int fn_bytes = fixnum_impl::FIXNUM_BYTES;
+    constexpr int bufsz = 4096;
+    uint8_t arr[bufsz];
+    int nelts;
+    for (auto &a : arr) a = 0;
+    res->retrieve_all(arr, bufsz, &nelts);
+
+    EXPECT_EQ(res->length(), n);
+    EXPECT_EQ(nelts, n);
+    EXPECT_LE(expected.size(), fn_bytes);
+
+    EXPECT_TRUE(arrays_are_equal(expected.data(), expected.size(), arr, nelts * fn_bytes));
+
+    delete fn;
+    delete res;
+    delete xx;
+    delete yy;
+}
+
+INSTANTIATE_TEST_CASE_P(add_cy_inst, add_cy_test,
+                        ::testing::ValuesIn(fname_to_args("tests/add_cy")));
 
 TEST(basic_arithmetic, square) {
     typedef default_fixnum_impl<16> fixnum_impl;
@@ -59,5 +225,6 @@ int main(int argc, char *argv[])
     testing::InitGoogleTest(&argc, argv);
     r = RUN_ALL_TESTS();
 
+    cout << "(Skipped " << skipped << " tests)." << endl;
     return r;
 }
