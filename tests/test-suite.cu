0d2f7caa31df589f84e590452a2df628701822d9
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-30 13:00:59 +1000

    Add mul_lo tests.

--- tests/test-suite.cu
+++ tests/test-suite.cu
@@ -169,6 +169,67 @@ TEST_P(add_cy_test, basic) {
 INSTANTIATE_TEST_CASE_P(add_cy_inst, add_cy_test,
                         ::testing::ValuesIn(fname_to_args("tests/add_cy")));
 
+template< typename fixnum_impl >
+struct mul_lo : public managed {
+    typedef typename fixnum_impl::fixnum fixnum;
+
+    __device__ void operator()(fixnum &r, fixnum a, fixnum b) {
+        fixnum_impl::mul_lo(r, a, b);
+    }
+};
+
+class mul_lo_test : public ::testing::TestWithParam<binop_args> { };
+
+TEST_P(mul_lo_test, basic) {
+    static constexpr size_t FIXNUM_BYTES = 256;
+    typedef default_fixnum_impl<FIXNUM_BYTES, uint64_t> fixnum_impl;
+    typedef fixnum_array<fixnum_impl> fixnum_array;
+
+    //const byte_array x, y, expected;
+    //tie(x, y, expected) = GetParam();
+    const binop_args &args = GetParam();
+    const byte_array &x = args[0];
+    const byte_array &y = args[1];
+    const byte_array &expected = args[2];
+
+    // FIXME: Should handle these cases somehow.
+    if (x.size() > FIXNUM_BYTES
+            || y.size() > FIXNUM_BYTES) {
+        ++skipped;
+        return;
+    }
+    size_t expected_len = std::min(expected.size(), FIXNUM_BYTES);
+
+    int n = 1;
+    auto res = fixnum_array::create(n);
+    auto xx = read_arg<fixnum_array>(x);
+    auto yy = read_arg<fixnum_array>(y);
+    auto fn = new mul_lo<fixnum_impl>();
+
+    fixnum_array::map(fn, res, xx, yy);
+
+    constexpr int fn_bytes = fixnum_impl::FIXNUM_BYTES;
+    constexpr int bufsz = 4096;
+    uint8_t arr[bufsz];
+    int nelts;
+    for (auto &a : arr) a = 0;
+    res->retrieve_all(arr, bufsz, &nelts);
+
+    EXPECT_EQ(res->length(), n);
+    EXPECT_EQ(nelts, n);
+    EXPECT_LE(expected_len, fn_bytes);
+
+    EXPECT_TRUE(arrays_are_equal(expected.data(), expected_len, arr, nelts * fn_bytes));
+
+    delete fn;
+    delete res;
+    delete xx;
+    delete yy;
+}
+
+INSTANTIATE_TEST_CASE_P(mul_lo_inst, mul_lo_test,
+                        ::testing::ValuesIn(fname_to_args("tests/mul_wide")));
+
 TEST(basic_arithmetic, square) {
     typedef default_fixnum_impl<16> fixnum_impl;
     typedef fixnum_array<fixnum_impl> fixnum_array;
