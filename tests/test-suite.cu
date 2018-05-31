bab5b1c5426a45a0898e093eafa7474adb2fed52
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-31 11:30:23 +1000

    Parameterise tests by type.

--- tests/test-suite.cu
+++ tests/test-suite.cu
@@ -60,7 +60,8 @@ public:
     }
 };
 
-vector<binop_args> fname_to_args(const char *fname) {
+vector<binop_args>
+fname_to_args(const char *fname) {
     ifstream file(fname);
     die_if ( ! file.good(), "Couldn't open file.");
     auto s = unique_ptr<const sexp>(sexp::create(file));
@@ -69,9 +70,11 @@ vector<binop_args> fname_to_args(const char *fname) {
     return v.result();
 }
 
-::testing::AssertionResult arrays_are_equal(
-        const uint8_t *expected, size_t expected_len,
-        const uint8_t *actual, size_t actual_len) {
+::testing::AssertionResult
+arrays_are_equal(
+    const uint8_t *expected, size_t expected_len,
+    const uint8_t *actual, size_t actual_len)
+{
     if (expected_len > actual_len) {
         return ::testing::AssertionFailure()
             << "arrays don't have the same length";
@@ -115,18 +118,44 @@ struct mul_lo : public managed {
     }
 };
 
-class Primitives : public ::testing::TestWithParam<int> { };
 
-TEST_P(Primitives, precomputed) {
-    static constexpr size_t FIXNUM_BYTES = 256;
-    typedef default_fixnum_impl<FIXNUM_BYTES, uint64_t> fixnum_impl;
+template< typename fixnum_impl_ >
+struct TypedPrimitives : public ::testing::Test {
+    typedef fixnum_impl_ fixnum_impl;
+
+    TypedPrimitives() {}
+};
+
+typedef ::testing::Types<
+    default_fixnum_impl<4, uint32_t>,
+    default_fixnum_impl<8, uint32_t>,
+    default_fixnum_impl<16, uint32_t>,
+    default_fixnum_impl<32, uint32_t>,
+    default_fixnum_impl<64, uint32_t>,
+    default_fixnum_impl<128, uint32_t>,
+
+    default_fixnum_impl<8, uint64_t>,
+    default_fixnum_impl<16, uint64_t>,
+    default_fixnum_impl<32, uint64_t>,
+    default_fixnum_impl<64, uint64_t>,
+    default_fixnum_impl<128, uint64_t>,
+    default_fixnum_impl<256, uint64_t>
+> FixnumImplTypes;
+
+TYPED_TEST_CASE(TypedPrimitives, FixnumImplTypes);
+
+template< typename fixnum_impl >
+void set_args_from_tcases(
+        const char *fname,
+        vector<binop_args> &tcases,
+        fixnum_array<fixnum_impl> *&res,
+        fixnum_array<fixnum_impl> *&xs,
+        fixnum_array<fixnum_impl> *&ys)
+{
+    static constexpr size_t FIXNUM_BYTES = fixnum_impl::FIXNUM_BYTES;
     typedef fixnum_array<fixnum_impl> fixnum_array;
-
-    int algo = GetParam();
-    const char *fname = algo == 0 ? "tests/add_cy" : "tests/mul_wide";
-    vector<binop_args> tcases = fname_to_args(fname);
+    tcases = fname_to_args(fname);
     int n = (int) tcases.size();
-    fixnum_array *xs, *ys, *res;
 
     xs = fixnum_array::create(n);
     ys = fixnum_array::create(n);
@@ -143,17 +172,15 @@ TEST_P(Primitives, precomputed) {
         r = ys->set(i, y.data(), y.size());
         ASSERT_EQ(r, std::min(y.size(), FIXNUM_BYTES));
     }
+}
 
-    if (algo == 0) {
-        auto fn_add = new add_cy<fixnum_impl>();
-        fixnum_array::map(fn_add, res, xs, ys);
-        delete fn_add;
-    } else {
-        auto fn_mul = new mul_lo<fixnum_impl>();
-        fixnum_array::map(fn_mul, res, xs, ys);
-        delete fn_mul;
-    }
-
+template< typename fixnum_impl >
+void check_result(
+        const vector<binop_args> &tcases,
+        const fixnum_array<fixnum_impl> *res)
+{
+    static constexpr size_t FIXNUM_BYTES = fixnum_impl::FIXNUM_BYTES;
+    int n = (int) tcases.size();
     EXPECT_EQ(res->length(), n);
     for (int i = 0; i < n; ++i) {
         const byte_array &expected = tcases[i][2];
@@ -165,63 +192,45 @@ TEST_P(Primitives, precomputed) {
         size_t expected_len = std::min(expected.size(), FIXNUM_BYTES);
         EXPECT_TRUE(arrays_are_equal(expected.data(), expected_len, arr, FIXNUM_BYTES));
     }
+}
 
+TYPED_TEST(TypedPrimitives, add_cy) {
+    typedef typename TestFixture::fixnum_impl fixnum_impl;
+    typedef fixnum_array<fixnum_impl> fixnum_array;
+
+    fixnum_array *res, *xs, *ys;
+    vector<binop_args> tcases;
+    set_args_from_tcases("tests/add_cy", tcases, res, xs, ys);
+
+    auto fn = new add_cy<fixnum_impl>();
+    fixnum_array::map(fn, res, xs, ys);
+    delete fn;
+
+    check_result(tcases, res);
     delete res;
     delete xs;
     delete ys;
 }
 
-INSTANTIATE_TEST_CASE_P(All_Primitives, Primitives, ::testing::Values(0, 1));
 
-
-TEST(basic_arithmetic, square) {
-    typedef default_fixnum_impl<16> fixnum_impl;
+TYPED_TEST(TypedPrimitives, mul_lo) {
+    typedef typename TestFixture::fixnum_impl fixnum_impl;
     typedef fixnum_array<fixnum_impl> fixnum_array;
 
-    int n = 1;
+    fixnum_array *res, *xs, *ys;
+    vector<binop_args> tcases;
+    set_args_from_tcases("tests/mul_wide", tcases, res, xs, ys);
 
-    auto res = fixnum_array::create(n);
-    auto arr1 = fixnum_array::create(n, 0UL);
-    auto arr2 = fixnum_array::create(n, ~0UL);
-    auto arr3 = fixnum_array::create(n, 245);
-    auto sqr = new square<fixnum_impl>();
+    auto fn = new mul_lo<fixnum_impl>();
+    fixnum_array::map(fn, res, xs, ys);
+    delete fn;
 
-    fixnum_array::map(sqr, res, arr1);
-    // check that res is all zeros
-    constexpr int fn_bytes = fixnum_impl::FIXNUM_BYTES;
-    constexpr int bufsz = 4096;
-    uint8_t arr[bufsz];
-    int nelts;
-    for (auto &a : arr) a = 0;
-    res->retrieve_all(arr, bufsz, &nelts);
-    EXPECT_EQ(nelts - res->length(), 0);
-    // NB: Only checking up to fn_bytes because n = 1;
-    for (int i = 0; i < fn_bytes; ++i)
-        EXPECT_EQ(arr[i], 0);
-
-    fixnum_array::map(sqr, res, arr2);
-    res->retrieve_all(arr, bufsz, &nelts);
-    EXPECT_EQ(nelts - res->length(), 0);
-    uint8_t res2[] = { 1, 0, 0, 0, 0, 0, 0, 0, 254, 255, 255, 255, 255, 255, 255, 255 };
-    // NB: Only checking up to fn_bytes because n = 1;
-    for (int i = 0; i < fn_bytes; ++i)
-        EXPECT_EQ(arr[i], res2[i]);
-
-    fixnum_array::map(sqr, res, arr3);
-    res->retrieve_all(arr, bufsz, &nelts);
-    EXPECT_EQ(nelts - res->length(), 0);
-    EXPECT_EQ(arr[0], 121);
-    EXPECT_EQ(arr[1], 234);
-    // NB: Only checking up to fn_bytes because n = 1;
-    for (int i = 2; i < fn_bytes; ++i)
-        EXPECT_EQ(arr[i], 0);
-
-    delete sqr;
+    check_result(tcases, res);
     delete res;
-    delete arr1;
-    delete arr2;
-    delete arr3;
+    delete xs;
+    delete ys;
 }
+
 
 int main(int argc, char *argv[])
 {
