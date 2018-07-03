3e696775d02ff130f9b07faf1588e436d2ca8014
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-07-03 15:43:15 +1000

    Implement 'count trailing zeroes' and [x]gcd functions.

--- src/util/primitives.cu
+++ src/util/primitives.cu
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <type_traits>
 
 /*
  * Low-level/primitive functions.
@@ -169,11 +170,158 @@ clz(uint64_t x) {
 }
 
 /*
- * Return 1 if x = 2^n for some n, 0 otherwise.
+ * Count Trailing Zeroes in x.
+ *
+ * Use __builtin_ctz{,l,ll}(x) or CUDA ASM depending on context.
  */
 __host__ __device__ __forceinline__
 int
-is_binary_power(uint32_t x) {
+ctz(uint32_t x) {
+#ifdef __CUDA_ARCH__
+    int n;
+    asm ("ctz.b32 %0, %1;" : "=r"(n) : "r"(x));
+    return n;
+#else
+    static_assert(sizeof(unsigned int) == sizeof(uint32_t),
+                  "attempted to use wrong __builtin_ctz{,l,ll}()");
+    return __builtin_ctz(x);
+#endif
+}
+
+__host__ __device__ __forceinline__
+int
+ctz(uint64_t x) {
+#ifdef __CUDA_ARCH__
+    int n;
+    asm ("ctz.b64 %0, %1;" : "=r"(n) : "l"(x));
+    return n;
+#else
+    static_assert(sizeof(unsigned long) == sizeof(uint64_t),
+                  "attempted to use wrong __builtin_ctz{,l,ll}()");
+    return __builtin_ctzl(x);
+#endif
+}
+
+/*
+ * GCD of x and y.
+ *
+ * Source: HAC Algorithm 14.54. Some ideas taken from
+ * https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
+ */
+template< typename int_type >
+__host__ __device__
+int_type
+gcd(int_type u, int_type v)
+{
+    typedef typename std::make_unsigned<int_type>::type uint_type;
+
+    if (u == 0) return v;
+    if (v == 0) return u;
+
+    // TODO: Strip zero bits as described by https://gmplib.org/manual/Binary-GCD.html#Binary-GCD
+
+    int g = ctz(static_cast<uint_type>(u | v));
+    u >>= ctz(static_cast<uint_type>(u));
+    do {
+        v >>= ctz(static_cast<uint_type>(v));
+        // TODO: Check whether removing the branch helps much here.
+        // Code would be something like
+        //   setp.gt.u32 %p, %u, %v
+        //   { .reg .u32 tmp
+        //     mov.b32  tmp, %v
+        //     selp.b32 %v, %v, %u, %p
+        //     selp.b32 %u, %u, tmp, %p }
+        if (u > v) {
+            int_type t = v;
+            v = u;
+            u = t;
+        }
+        v = v - u;
+    } while (v != 0);
+
+    return u << g;
+}
+
+
+/*
+ * Extended GCD of x and y.
+ *
+ * Source: HAC Algorithm 14.61. Some ideas due to
+ * https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
+ *
+ * FIXME: Not quite sure why, but when x > y, the cofactors returned are not the
+ * optimal ones. If it's important to get the optimal cofactors, make sure x < y.
+ */
+template< typename int_type >
+__host__ __device__
+int_type
+xgcd(
+    int_type &a, int_type &b,
+    int_type x, int_type y)
+{
+    static_assert(std::is_signed<int_type>::value == true,
+                  "template type must be signed");
+    typedef typename std::make_unsigned<int_type>::type uint_type;
+
+    // TODO: Specialise to case of m odd to avoid some calculations as described
+    // in HAC 14.64. See also errata: http://cacr.uwaterloo.ca/hac/errata/errata.html
+
+    // TODO: Strip zero bits as described by https://gmplib.org/manual/Binary-GCD.html#Binary-GCD
+    int_type u, v, g;
+    int_type A = 1, B = 0, C = 0, D = 1;
+
+    if (x == 0) { a = C; b = D; return y; }
+    if (y == 0) { a = A; b = B; return x; }
+
+    g = ctz(static_cast<uint_type>(x | y));
+    x >>= g;
+    y >>= g;
+    u = x;
+    v = y;
+
+    auto reduce = [x, y] (int_type &t, int_type &G, int_type &H) {
+        // TODO: Try speeding this up by treating ctz(t) bits at a time.
+        while ( ! (t & 1)) {
+            t >>= 1;
+            // -((G | H) & 1) is 0 if G and H are even, and 0xffffffff... if G
+            // or H is odd, because -1 in two's complement is all bits on.
+            int_type odd_mask = -((G | H) & 1);
+
+            G = (G + (odd_mask & y)) >> 1;
+            H = (H - (odd_mask & x)) >> 1;
+        }
+    };
+
+    do {
+        reduce(u, A, B);
+        reduce(v, C, D);
+        // TODO: Find a way to avoid this branch.
+        if (u >= v) {
+            u -= v;
+            A -= C;
+            B -= D;
+        } else {
+            v -= u;
+            C -= A;
+            D -= B;
+        }
+    } while (u != 0);
+
+    a = C;
+    b = D;
+    return v << g;
+}
+
+/*
+ * Return 1 if x = 2^n for some n, 0 otherwise. (Caveat: Returns 1 for x = 0
+ * which is not a binary power.)
+ */
+template< typename uint_type >
+__host__ __device__ __forceinline__
+int
+is_binary_power(uint_type x) {
+    static_assert(std::is_unsigned<uint_type>::value == true,
+                  "template type must be unsigned");
     return ! (x & (x - 1));
 }
 
