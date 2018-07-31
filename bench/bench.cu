2d9b576030c062650b92daa1b9a882383b39156b
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-07-31 15:06:57 +1000

    First pass at improving the benchmark.

--- bench/bench.cu
+++ bench/bench.cu
@@ -1,18 +1,64 @@
-#include <iostream>
+#include <cstdio>
 #include <cstring>
 #include <cassert>
 
-#include "fixnum/default.cu"
+#include "fixnum/warp_fixnum.cu"
 #include "array/fixnum_array.h"
-#include "functions/square.cu"
-#include "functions/ec_add.cu"
+#include "functions/modexp.cu"
 
 using namespace std;
 
-template< int fn_bytes, typename word_tp = uint32_t >
+template< typename fixnum >
+struct my_modexp {
+    __device__ void operator()(fixnum &z, fixnum x, fixnum e, fixnum m) {
+        fixnum zz;
+        modexp<fixnum> me(m, e);
+        me(zz, x);
+        z = zz;
+    };
+};
+
+template< typename fixnum >
+struct mul_lo {
+    __device__ void operator()(fixnum &r, fixnum a, fixnum b) {
+        fixnum s;
+        fixnum::mul_lo(s, a, b);
+        r = s;
+    }
+};
+
+template< typename fixnum >
+struct mul_wide {
+    __device__ void operator()(fixnum &s, fixnum &r, fixnum a, fixnum b) {
+        fixnum rr, ss;
+        fixnum::mul_wide(ss, rr, a, b);
+        s = ss;
+        r = rr;
+    }
+};
+
+template< template <typename> class Func, typename fixnum, typename ...Args >
+void bench_func(const char *fn_name, Args ...args) {
+    typedef fixnum_array<fixnum> fixnum_array;
+
+    // warm up
+    fixnum_array::template map<Func>(args...);
+
+    clock_t c = clock();
+    fixnum_array::template map<Func>(args...);
+    c = clock() - c;
+
+    int nelts = std::min( { args->length()... } ); // should be all the same
+    double secinv = (double)CLOCKS_PER_SEC / c;
+    printf("%9s:  %6.4f  %9.1f\n",
+           fn_name, 1/secinv, nelts * 1e-3 * secinv);
+
+}
+
+template< int fn_bytes, typename word_fixnum >
 void bench(size_t nelts) {
-    typedef default_fixnum_impl<fn_bytes, word_tp> fixnum_impl;
-    typedef fixnum_array<fixnum_impl> fixnum_array;
+    typedef warp_fixnum<fn_bytes, word_fixnum> fixnum;
+    typedef fixnum_array<fixnum> fixnum_array;
 
     uint8_t *input = new uint8_t[fn_bytes * nelts];
     for (size_t i = 0; i < fn_bytes * nelts; ++i)
@@ -22,18 +68,16 @@ void bench(size_t nelts) {
     in = fixnum_array::create(input, fn_bytes * nelts, fn_bytes);
     res = fixnum_array::create(nelts);
 
-    typedef square<fixnum_impl> square;
-    auto fn = unique_ptr<square>(new square);
+    double total_MiB = fixnum::BYTES * (double)nelts / (1 << 20);
+    printf("fixnum: %4d bits (%2d-bit digits); #elts: %5de3, total data: %.2f MiB\n",
+           fixnum::BITS, fixnum::digit::BITS, (int)(nelts * 1e-3), total_MiB);
+    printf("            seconds   Kops/s\n");
+    bench_func<mul_lo, fixnum>("mul_lo", res, in, in);
+    bench_func<mul_wide, fixnum>("mul_wide", res, res, in, in);
+    //bench_func<mul_mod>("mul_mod", res, in, in);
+    bench_func<my_modexp, fixnum>("modexp", res, in, in, in);
 
-    clock_t c = clock();
-    fixnum_array::map(fn.get(), res, in);
-    c = clock() - c;
-    double total_MiB = fn_bytes * (double)nelts / (1 << 20);
-    cout << nelts << " elts, "
-         << setw(3) << fn_bytes << " (" << sizeof(word_tp) << ") bytes per element: "
-         << total_MiB * (double)CLOCKS_PER_SEC / c
-         << " MiB/s (take with grain of salt)"
-         << endl;
+    puts("");
 
     delete in;
     delete res;
@@ -41,59 +85,23 @@ void bench(size_t nelts) {
 }
 
 int main(int argc, char *argv[]) {
-    long n = 16, m = 1;
+    long m = 1;
     if (argc > 1)
-        n = atol(argv[1]);
+        m = atol(argv[1]);
 
-    if (argc > 2)
-        m = atol(argv[2]);
+    bench<8, u32_fixnum>(m);
+    bench<16, u32_fixnum>(m);
+    bench<32, u32_fixnum>(m);
+    bench<64, u32_fixnum>(m);
+    bench<128, u32_fixnum>(m);
 
-    typedef default_fixnum_impl<16> fixnum_impl;
-    typedef fixnum_array<fixnum_impl> fixnum_array;
-
-    auto res = fixnum_array::create(n);
-    auto arr1 = fixnum_array::create(n, ~0UL);
-    auto arr2 = fixnum_array::create(n, 245);
-
-    cout << "res  = " << res << endl;
-    cout << "arr1 = " << arr1 << endl;
-    cout << "arr2 = " << arr2 << endl;
-
-    // FIXME: How do I return cy without allocating a gigantic array
-    // where each element is only 0 or 1? Need fixnum_array to have
-    // C-array semantics, hence allowing other C arrays to be used
-    // alongside, so pass an int* to collect the carries.
-
-    auto fn1 = new ec_add<fixnum_impl>();
-    fixnum_array::map(fn1, res, arr1, arr2);
-    auto fn2 = new square<fixnum_impl>();
-    fixnum_array::map(fn2, res, arr1);
-
-    delete fn1;
-    delete fn2;
-
-    cout << "res  = " << res << endl;
-    cout << "arr1 = " << arr1 << endl;
-    cout << "arr2 = " << arr2 << endl;
-
-    cout << endl << "uint32:" << endl;
-    bench<8>(m);
-    bench<16>(m);
-    bench<32>(m);
-    bench<64>(m);
-    bench<128>(m);
-
-    cout << endl << "uint64:" << endl;
-    bench<8, uint64_t>(m);
-    bench<16, uint64_t>(m);
-    bench<32, uint64_t>(m);
-    bench<64, uint64_t>(m);
-    bench<128, uint64_t>(m);
-    bench<256, uint64_t>(m);
-
-    delete res;
-    delete arr1;
-    delete arr2;
+    puts("");
+    bench<8, u64_fixnum>(m);
+    bench<16, u64_fixnum>(m);
+    bench<32, u64_fixnum>(m);
+    bench<64, u64_fixnum>(m);
+    bench<128, u64_fixnum>(m);
+    bench<256, u64_fixnum>(m);
 
     return 0;
 }
