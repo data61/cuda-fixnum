ce9074591e3b21683e7bbc492e0e1e9626248abf
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-08-17 16:10:41 +1000

    Complete-ish version of sqr_wide.

--- src/fixnum/internal/warp_fixnum_impl.cu
+++ src/fixnum/internal/warp_fixnum_impl.cu
@@ -74,39 +74,71 @@ namespace internal {
      */
     template< typename layout >
     __device__ void
-    sqr_lo(fixnum &r, fixnum a)
+    sqr_wide(fixnum &ss, fixnum &rr, fixnum a)
     {
-        int L = layout::laneIdx(), T = layout::topLaneIdx;
-        fixnum cy = 0;
-        fixnum a1, a2;
+        int L = layout::laneIdx();
 
-        // WARNING: See section "Dynamic Indexing with Non-Uniform Access"
-        // here:
+        fixnum r, s;
+        r = fixnum::zero();
+        s = fixnum::zero();
+        digit cy = digit::zero();
+
+        // WARNING: See section "Dynamic Indexing with Non-Uniform Access" here:
         // https://devblogs.nvidia.com/parallelforall/fast-dynamic-indexing-private-arrays-cuda/
-        // Suggests that it might be wise to put threadmap in shared memory.
+        // Suggests that it might be wise to put tblmap_* in shared memory.
         for (int i = 0; i < layout::WIDTH / 2; ++i) {
-            // TODO: Investigate whether we are actually traversing this array
-            // in the "right" way according to its memory layout. Might we want
-            // to take a "transpose" so that we iterate over DIGITREMAP[0][L][i]
-            // for example?
-            int src1 = DIGITREMAP[i][0][L];
-            int src2 = DIGITREMAP[i][1][L];
-            a1 = (L == T) ? 0 : shfl(a, src1);
-            a2 = (L == T) ? 0 : shfl(a, src2);
+            fixnum a1, a2;
+            // ti = tblmap[i][L] is the lane for this iteration of squaring.
+            a1 = get(a, tblmap_lhs[i][L]);
+            a2 = get(a, tblmap_rhs[i][L]);
 
-            internal::mad_hi_cc(r, cy, a1, a2, r);
-            r = shfl_up0(r, 1);
-            cy = shfl_up0(cy, 1);
-            internal::mad_lo_cc(r, cy, a1, a2, r);
+            digit::mad_lo_cc(s, a1, a2, s);
+
+            fixnum s0 = get(s, 0);
+            r = (L == i) ? s0 : r; // r[i] = s[0]
+            s = layout::shfl_down0(s, 1);
+
+            digit::addc_cc(s, s, cy);  // add carry from prev digit
+            digit::addc(cy, 0, 0);     // cy = CC.CF
+            digit::mad_hi_cy(s, cy, a1, a2, s);
         }
 
-        cy = shfl_up(cy, 1);
-        fixnum::add(r, r, cy);
+        cy = layout::shfl_up0(cy, 1);
+        add(s, s, cy);
 
-        // FIXME: Still need to calculate and add the squares on diagonal.
+        fixnum overflow;
+        lshift(s, s, digit::BITS + 1);  // s *= 2
+        lshift(r, overflow, r, digit::BITS + 1);  // r *= 2
+        // Propagate r overflow to s
+        add_cy(s, cy, s, overflow); // really a logior, since s was just lshifted.
+        assert(digit::is_zero(cy));
 
-        // r *= 2
-        fixnum::lshift(r, r, 1);
+        // Calculate and add the squares on the diagonal. NB: by parallelising
+        // the multiplication like this then shuffling the results, we lose the
+        // ability to use a fused multiply-and-add call.
+        digit::mul_wide(ai_sqr_hi, ai_sqr_lo, a, a);
+
+        // NB: This version saves one shuffle at the expense of more complicated
+        // lane calculations (~5 vs ~2 bitops). Alternative version is in a
+        // comment below.
+        int is_hi_half = L >= layout::WIDTH / 2;
+        int lane_shift = (L << 1) & (layout::WIDTH - 1); // 2*L % width;
+        t_lo = layout::shfl(ai_sqr_lo, lane_shift + is_hi_half);
+        t_hi = layout::shfl(ai_sqr_hi, lane_shift + !is_hi_half);
+        r_diag = (L & 1) ? t_hi : t_lo;
+        t_hi = (L & 1) ? t_lo : t_hi;
+        s_diag = layout::shfl(t_hi, L^1U); // switch odd and even lanes
+#if 0
+        t_lo = layout::shfl(ai_sqr_lo, L / 2);
+        t_hi = layout::shfl(ai_sqr_hi, L / 2);
+        r_diag = (L & 1) ? t_hi : t_lo;
+        t_lo = layout::shfl(ai_sqr_lo, (L + layout::WIDTH) / 2);
+        t_hi = layout::shfl(ai_sqr_hi, (L + layout::WIDTH) / 2);
+        s_diag = (L & 1) ? t_hi : t_lo;
+#endif
+
+        add_cy(r, cy, r, r_diag);
+        add(s, s, s_diag);
+        add(s, s, cy);
     }
-
 }
