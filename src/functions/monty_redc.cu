163421e7d138ca1c8b767dd3c1ad9b161aa9285a
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-08-24 14:15:43 +1000

    Finish fixes to REDC.

--- src/functions/monty_redc.cu
+++ src/functions/monty_redc.cu
@@ -2,31 +2,44 @@
 
 template< typename fixnum >
 class monty_redc {
-    typedef typename fixnum::digit digit;
-
     // Modulus for Monty arithmetic
     fixnum mod;
-    // inv_mod * mod = -1 % 2^digit::BITS.
-    digit  inv_mod;
+    // inv_mod * mod = -1 % 2^fixnum::BITS.
+    fixnum inv_mod;
 
 public:
-    __device__ void operator()(fixnum &s, fixnum r_hi, fixnum r_lo) {
-        static constexpr int T = fixnum::layout::toplaneIdx;
-        static constexpr int WIDTH = fixnum::SLOT_WIDTH;
-        int L = slot_layout::laneIdx();
-        digit x, msw;
+    __device__ monty_redc(fixnum mod_)
+    : mod(mod_) {
+        modinv<fixnum> minv;
+        minv(inv_mod, mod, fixnum::BITS);
+        fixnum::neg(inv_mod);
+    }
 
-        x = r_lo;
-        for (int i = 0; i < WIDTH; ++i) {
-            digit u = fixnum::get(r_lo, i) * inv_mod;
-            msw = fixnum::addmuli(x, mod, u);
+    __device__ void operator()(fixnum &r, fixnum a_hi, fixnum a_lo) {
+        fixnum b, s_hi, s_lo;
+        digit cy;
 
-            r_lo = (L == i) ? msw : r_lo;
-            x = layout::shfl_down0(x, 1);
-            r_hi_i = fixnum::get(r_hi, i);
-            x = (L == T) ? r_hi_i : x;
-        }
-        fixnum::add_cy(s, msw, x, r_lo);
-        normalise(s, msw, mod);
+        fixnum::mul_lo(b, a_lo, inv_mod);
+
+        // This section is essentially s = floor(mad_wide(b, mod, a) / R)
+
+        // TODO: Can we employ the trick to avoid a multiplication because we
+        // know b = am' (mod R)?
+        fixnum::mul_wide(s_hi, s_lo, b, mod);
+        // TODO: Only want the carry; find a cheaper way to determine that
+        // without doing the full addition.
+        fixnum::add_cy(s_lo, cy, s_lo, a_lo);
+#ifndef NDEBUG
+        // NB: b = am' (mod R) => a + bm = a + amm' = 2a (mod R). So surely
+        // all I need to propagate is the top bit of a_lo?
+        fixnum top_bit, dummy;
+        fixnum::lshift(dummy, top_bit, a_lo, 1);
+        assert(digit::cmp(cy, top_bit) == 0);
+#endif
+        fixnum::add(r, s_hi, a_hi);
+        fixnum::add(r, r, cy);
+
+        if (fixnum::cmp(r, mod) >= 0)
+            sub(r, r, mod);
     }
 };
